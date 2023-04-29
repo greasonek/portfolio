895a9c6538d2fff6fcd2d6723e6e52ac4e809165
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-04-29 14:48:02 -0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,36 +21,36 @@ class Footer extends Component {
             <div className="col social-links">{networks}</div>
             <div className="col">
               <div 
-                // onClick={() =>
-                //   this.props.applyPickedLanguage(
-                //     window.$primaryLanguage,
-                //     window.$secondaryLanguageIconId
-                //   )
-                // }
-                style={{ display: "inline" }}
-              >
-                {/* <span
-                  className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
-                  data-inline="false"
-                  id={window.$primaryLanguageIconId}
-                ></span> */}
-              </div>
-              <div 
                 onClick={() =>
                   this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
-                    window.$primaryLanguageIconId
+                    window.$primaryLanguage,
+                    window.$secondaryLanguageIconId
                   )
                 }
                 style={{ display: "inline" }}
               >
-                {/* <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
+                <span
+                  // className="iconify language-icon mr-5"
+                  // data-icon="twemoji-flag-for-flag-united-kingdom"
+                  // data-inline="false"
+                  id={window.$primaryLanguageIconId}
+                ></span>
+              </div>
+              <div 
+                // onClick={() =>
+                //   this.props.applyPickedLanguage(
+                //     window.$secondaryLanguage,
+                //     window.$primaryLanguageIconId
+                //   )
+                // }
+                style={{ display: "inline" }}
+              >
+                <span
+                  // className="iconify language-icon"
+                  // data-icon="twemoji-flag-for-flag-poland"
+                  // data-inline="false"
                   id={window.$secondaryLanguageIconId}
-                ></span> */}
+                ></span>
               </div>
             </div>
             <div className="col">
